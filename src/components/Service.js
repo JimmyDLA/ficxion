f4c67d922da637cf0ec8a0a732d0607a0e586598
Jimmy De Los Angeles <jimmydelosangeles@me.com>
2020-11-12 20:02:09 -0500

    wip; service done ready for projects

--- src/components/Service.js
+++ src/components/Service.js
@@ -1,7 +1,9 @@
 import './__style__.css';
 import iphone from '../assets/images/iphone_x2.png';
 import macbook from '../assets/images/macbook.png';
+import macDesktop from '../assets/images/mac_desktop.png';
 import webpage from '../assets/images/webpage.png';
+import webpage2 from '../assets/images/webpage2.png';
 
 export const Service = () => {
 
@@ -31,11 +33,27 @@ export const Service = () => {
                     <div id="serviceBodyContBottom">
                         <h5 id="serviceTitle">Smart</h5>
                         <h5 id="serviceTitle">Designs</h5>
-                        <p id="sericeBody">
+                        <p id="serviceBodyBottom">
                             Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, 
                             vestibulum in vulputate at, tempus viverra turpis.
                         </p>
                     </div>
+                </div>
+            </div>
+            <div className="lastSection" id="serviceTop">
+                <div id="serviceTopLeft">
+                    <div id="serviceBodyCont">
+                        <h5 id="serviceTitle">Creative</h5>
+                        <h5 id="serviceTitle">Solutions</h5>
+                        <p id="serviceBody">
+                            Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, 
+                            vestibulum in vulputate at, tempus viverra turpis.
+                        </p>
+                    </div>
+                </div>
+                <div id="serviceTopRight">
+                    <img height="450" src={macDesktop} />
+                    <img id="webpage2" src={webpage2} height="290" width="510" />
                 </div>
             </div>
         </div>
