3949988582fc84cf83273d0321a1a4ae64fe824b
Jimmy De Los Angeles <jimmydelosangeles@me.com>
2021-08-13 17:30:35 -0400

    new develop branch

--- src/routers/Home.js
+++ src/routers/Home.js
@@ -6,28 +6,31 @@ import { Projects } from '../components/Projects.js';
 import { Contact } from '../components/Contact.js';
 import video from '../assets/videos/tech_global.mp4';
 import logo from '../assets/images/tech_logo2.png';
+
+const Landing = () => (
+    <div className="container">
+      <div className="content">
+        <div id="logoContainer">
+          <img src={logo} alt="logo" id="logo" width="80" height="80" />
+          <h1 className="title">Ficxion Dev.</h1>
+        </div>
+
+        <h2>Making Imagination, Reality </h2>
+      </div>
+      <video autoPlay muted loop id="myVideo">
+          <source id="src" src={video} type="video/mp4" />
+      </video>
+    </div>
+)
 export const Home = () => {
     return (
-        <div className="superCont">
-        <div className="container">
-
-            <div className="content">
-                <div id="logoContainer">
-                    <img src={logo} alt="logo" id="logo" width="80" height="80" />
-                    <h1 className="title">Ficxion Dev.</h1>
-                </div>
-
-                <h2>Making Imagination, Reality </h2>
-            </div>
-            <video autoPlay muted loop id="myVideo">
-                <source id="src" src={video} type="video/mp4" />
-            </video>
-        </div>
+      <div className="superCont">
+        <Landing />
         <NavBar />
         <Overview />
         <Service />
         <Projects />
         <Contact />
-        </div>
+      </div>
     );
 }
